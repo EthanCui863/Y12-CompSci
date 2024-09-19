d9f9000a319e198f80ef26f65187f8a5d94d63e5
EthanCui863 <142461548+EthanCui863@users.noreply.github.com>
2024-09-20 11:55:33 +1200

    Bases of the website (V1.1)
    Added progress bar and dark mode, logo has been temporarily changed to just the name of the website.

---
+++ base/base.js
@@ -0,0 +1,32 @@
+const filled = document.querySelector(".filled");
+
+function update() {
+  filled.style.width = `${
+    (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
+  }%`;
+  requestAnimationFrame(update);
+}
+
+update();
+
+let darkmode = localStorage.getItem("darkmode");
+const themeSwitch = document.getElementById("theme-switch");
+
+const enableDarkmode = () => {
+  document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
+};
+
+const disableDarkmode = () => {
+  document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
+};
+
+if (darkmode === "active") enableDarkmode();
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
+
+// website code
